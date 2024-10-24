c32b4eb484210dc02b10d8ef9eb718a19ea981a1
KhoaLA8 <>
2024-10-24 18:07:42 +0700

    update endpoint

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   appName: "Udagram",
   apiHost:
-    "http://udagram-api-dev.eba-cpvxvnfp.us-east-1.elasticbeanstalk.com/api/v0",
+    "http://udagram-be.eba-4htvpk2j.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
